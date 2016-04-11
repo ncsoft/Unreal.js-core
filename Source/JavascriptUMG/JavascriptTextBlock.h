7ba3399651af840d87157ef3260722e078c84fa5
Nako Sung <deif79@gmail.com>
2016-04-12 04:33:03 +0900

    new widgets

---
+++ Source/JavascriptUMG/JavascriptTextBlock.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Components/TextBlock.h"
+#include "JavascriptTextBlock.generated.h"
+
+class SSearchBox;
+
+UCLASS(Experimental)
+class JAVASCRIPTUMG_API UJavascriptTextBlock : public UTextBlock
+{
+	GENERATED_UCLASS_BODY()
+
+public:		
+	/** Highlight text that appears when there is no text in the text box */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Content)
+	FText HighlightText;
+
+	/** A bindable delegate to allow logic to drive the Highlight text of the widget */
+	UPROPERTY()
+	FGetText HighlightTextDelegate;
+
+	// UWidget interface
+	virtual void SynchronizeProperties() override;
+	// End of UWidget interface
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+	void SetHighlightText(FText InHighlightText);
+};
