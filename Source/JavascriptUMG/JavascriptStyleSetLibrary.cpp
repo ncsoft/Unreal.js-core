d04a5139f0d3ca1758521d84c7b4534e1073ea69
Nako Sung <deif79@gmail.com>
2016-04-17 21:36:48 +0900

    StyleSet accessor moved into JavascriptUMG from JavascriptEditor

---
+++ Source/JavascriptUMG/JavascriptStyleSetLibrary.cpp
@@ -0,0 +1,179 @@
+#include "JavascriptUMG.h"
+#include "JavascriptStyleSetLibrary.h"
+
+static UStruct* Struct_SlateColor;
+static UStruct* Struct_ButtonStyle;
+static UStruct* Struct_SlateBrush;
+static UStruct* Struct_TextBlockStyle;
+static UStruct* Struct_EditableTextStyle;
+static UStruct* Struct_EditableTextBoxStyle;
+static UStruct* Struct_CheckBoxStyle;
+static UStruct* Struct_ComboBoxStyle;
+static UStruct* Struct_ComboButtonStyle;
+
+void Prepare()
+{
+	if (Struct_SlateColor) return;	
+
+	auto prepare = [&](UStruct*& Out, const TCHAR* Name)
+	{
+		UStructProperty* Prop = FindField<UStructProperty>(UJavascriptStyleSetLibrary::StaticClass(), Name);
+		Out = Prop->Struct;
+	};
+
+	prepare(Struct_SlateColor, TEXT("SlateColor"));
+	prepare(Struct_ButtonStyle, TEXT("ButtonStyle"));
+	prepare(Struct_SlateBrush, TEXT("SlateBrush"));
+	prepare(Struct_TextBlockStyle, TEXT("TextBlockStyle"));
+	prepare(Struct_EditableTextStyle, TEXT("EditableTextStyle"));
+	prepare(Struct_EditableTextBoxStyle, TEXT("EditableTextBoxStyle"));
+	prepare(Struct_CheckBoxStyle, TEXT("CheckBoxStyle"));
+	prepare(Struct_ComboBoxStyle, TEXT("ComboBoxStyle"));
+	prepare(Struct_ComboButtonStyle, TEXT("ComboButtonStyle"));
+}
+
+FSlateColor Fixup(const FSlateColor& Src)
+{
+	Prepare();
+
+	if (Src.IsColorSpecified())
+	{
+		return FSlateColor(Src.GetSpecifiedColor());
+	}
+	else
+	{
+		return Src;
+	}
+}
+
+void FixupStruct(UStruct* Struct, void* Dest, const void* Src)
+{
+	if (Struct == Struct_SlateColor)
+	{
+		*((FSlateColor*)Dest) = Fixup(*(FSlateColor*)Src);
+		return;
+	}
+
+	for (TFieldIterator<UProperty> PropertyIt(Struct, EFieldIteratorFlags::IncludeSuper); PropertyIt; ++PropertyIt)
+	{
+		auto Property = *PropertyIt;
+		if (UStructProperty* p = Cast<UStructProperty>(Property))
+		{
+			FixupStruct(p->Struct, p->ContainerPtrToValuePtr<void>(Dest), p->ContainerPtrToValuePtr<void>(Src));
+		}
+	}
+}
+
+FSlateBrush Fixup(const FSlateBrush& Src)
+{
+	Prepare();
+
+	FSlateBrush Dest = Src;
+	FixupStruct(Struct_SlateBrush, &Dest, &Src);
+	return Dest;
+}
+
+static const ISlateStyle& GetStyleSet(const FJavascriptStyleSet& Handle)
+{
+	auto StyleSet = FSlateStyleRegistry::FindSlateStyle(Handle.StyleSetName);
+	if (StyleSet)
+	{
+		return *StyleSet;
+	}
+	else
+	{
+		static TSharedPtr<FSlateStyleSet> Default;
+		if (!Default.IsValid()) 
+		{
+			Default = MakeShareable(new FSlateStyleSet(FName()));
+		}
+		return *(Default.Get());
+	}
+}
+
+template <typename T>
+T FixupWidgetStyle(const FJavascriptStyleSet& Handle, UStruct* Struct, const FName& StyleName)
+{
+	Prepare();
+
+	const auto& Src = GetStyleSet(Handle).GetWidgetStyle<T>(StyleName);
+	T Dest = Src;
+	FixupStruct(Struct, &Dest, &Src);
+	return Dest;
+}
+
+FButtonStyle UJavascriptStyleSetLibrary::GetButtonStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FButtonStyle>(Handle, Struct_ButtonStyle, StyleName);
+}
+
+FTextBlockStyle UJavascriptStyleSetLibrary::GetTextBlockStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FTextBlockStyle>(Handle, Struct_TextBlockStyle, StyleName);
+}
+
+FEditableTextStyle UJavascriptStyleSetLibrary::GetEditableTextStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FEditableTextStyle>(Handle, Struct_EditableTextStyle, StyleName);
+}
+
+FEditableTextBoxStyle UJavascriptStyleSetLibrary::GetEditableTextBoxStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FEditableTextBoxStyle>(Handle, Struct_EditableTextBoxStyle, StyleName);
+}
+
+FCheckBoxStyle UJavascriptStyleSetLibrary::GetCheckBoxStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FCheckBoxStyle>(Handle, Struct_CheckBoxStyle, StyleName);
+}
+
+FComboBoxStyle UJavascriptStyleSetLibrary::GetComboBoxStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FComboBoxStyle>(Handle, Struct_ComboBoxStyle, StyleName);
+}
+
+FComboButtonStyle UJavascriptStyleSetLibrary::GetComboButtonStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return FixupWidgetStyle<FComboButtonStyle>(Handle, Struct_ComboButtonStyle, StyleName);
+}
+
+FMargin UJavascriptStyleSetLibrary::GetMargin(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return GetStyleSet(Handle).GetMargin(StyleName);
+}
+
+FSlateColor UJavascriptStyleSetLibrary::GetSlateColor(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return Fixup(GetStyleSet(Handle).GetSlateColor(StyleName));
+}
+
+FSlateBrush UJavascriptStyleSetLibrary::GetBrush(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	auto Brush = GetStyleSet(Handle).GetBrush(StyleName);
+	return Brush ? Fixup(*Brush) : FSlateBrush();
+}
+
+FLinearColor UJavascriptStyleSetLibrary::GetColor(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return GetStyleSet(Handle).GetColor(StyleName);
+}
+
+FVector2D UJavascriptStyleSetLibrary::GetVector(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return GetStyleSet(Handle).GetVector(StyleName);
+}
+
+float UJavascriptStyleSetLibrary::GetFloat(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return GetStyleSet(Handle).GetFloat(StyleName);
+}
+
+FSlateFontInfo UJavascriptStyleSetLibrary::GetFontStyle(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return GetStyleSet(Handle).GetFontStyle(StyleName);
+}
+
+FSlateSound UJavascriptStyleSetLibrary::GetSound(const FJavascriptStyleSet& Handle, const FName& StyleName)
+{
+	return GetStyleSet(Handle).GetSound(StyleName);
+}
---
+++ Source/JavascriptUMG/JavascriptStyleSetLibrary.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "JavascriptStyleSetLibrary.generated.h"
+
+USTRUCT()
+struct FJavascriptStyleSet
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FName StyleSetName;
+};
+/**
+ * 
+ */
+UCLASS()
+class JAVASCRIPTUMG_API UJavascriptStyleSetLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:		
+
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FButtonStyle GetButtonStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FTextBlockStyle GetTextBlockStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FEditableTextStyle GetEditableTextStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FEditableTextBoxStyle GetEditableTextBoxStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FCheckBoxStyle GetCheckBoxStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FComboBoxStyle GetComboBoxStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FComboButtonStyle GetComboButtonStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FMargin GetMargin(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FSlateColor GetSlateColor(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FSlateBrush GetBrush(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FLinearColor GetColor(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FVector2D GetVector(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static float GetFloat(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FSlateFontInfo GetFontStyle(const FJavascriptStyleSet& Handle, const FName& StyleName);
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	static FSlateSound GetSound(const FJavascriptStyleSet& Handle, const FName& StyleName);
+
+	UPROPERTY()
+	FSlateColor SlateColor;	
+
+	UPROPERTY()
+	FSlateBrush SlateBrush;
+
+	UPROPERTY()
+	FButtonStyle ButtonStyle;
+	
+	UPROPERTY()
+	FTextBlockStyle TextBlockStyle;
+
+	UPROPERTY()
+	FEditableTextStyle EditableTextStyle;
+
+	UPROPERTY()
+	FEditableTextBoxStyle EditableTextBoxStyle;
+
+	UPROPERTY()
+	FCheckBoxStyle CheckBoxStyle;
+
+	UPROPERTY()
+	FComboBoxStyle ComboBoxStyle;
+
+	UPROPERTY()	
+	FComboButtonStyle ComboButtonStyle;
+};
