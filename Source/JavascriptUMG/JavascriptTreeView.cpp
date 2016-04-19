05ddcb95429f83be5d60f4748e03cc222e09d9d7
Nako Sung <nakosung@ncsoft.com>
2016-04-19 10:20:07 +0900

    TreeView

---
+++ Source/JavascriptUMG/JavascriptTreeView.cpp
@@ -0,0 +1,213 @@
+#include "JavascriptUMG.h"
+#include "JavascriptTreeView.h"
+#include "JavascriptContext.h"
+
+UJavascriptTreeView::UJavascriptTreeView(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{	
+	bIsVariable = true;
+
+	SelectionMode = ESelectionMode::Single;
+}
+
+TSharedRef<SWidget> UJavascriptTreeView::RebuildWidget()
+{
+	TSharedPtr<SHeaderRow> HeaderRowWidget;
+	if (Columns.Num())
+	{
+		HeaderRowWidget = SNew(SHeaderRow);
+
+		for (auto& Column : Columns)
+		{
+			if (!Column.Widget)
+			{
+				if (OnGetColumn.IsBound())
+				{
+					Column.Widget = OnGetColumn.Execute(Column.Id, this);
+				}
+				else
+				{
+					continue;
+				}
+			}
+
+			HeaderRowWidget->AddColumn(
+				SHeaderRow::Column(Column.Id)
+				.FillWidth(Column.Width)
+				[
+					Column.Widget->TakeWidget()
+				]
+			);
+		}
+	}
+	MyTreeView = SNew(STreeView< UObject* >)
+		.SelectionMode(SelectionMode)
+		.TreeItemsSource(&Items)
+		.OnGenerateRow(BIND_UOBJECT_DELEGATE(STreeView< UObject* >::FOnGenerateRow, HandleOnGenerateRow))
+		.OnGetChildren(BIND_UOBJECT_DELEGATE(STreeView< UObject* >::FOnGetChildren, HandleOnGetChildren))
+		.OnSelectionChanged_Lambda([this](UObject* Object, ESelectInfo::Type SelectInfo) {
+			OnSelectionChanged(Object, SelectInfo);
+		})
+		.OnMouseButtonDoubleClick_Lambda([this](UObject* Object) {
+			OnDoubleClick(Object);
+		})
+		.HeaderRow(HeaderRowWidget)
+		//.OnContextMenuOpening(this, &SSocketManager::OnContextMenuOpening)
+		//.OnItemScrolledIntoView(this, &SSocketManager::OnItemScrolledIntoView)
+		//	.HeaderRow
+		//	(
+		//		SNew(SHeaderRow)
+		//		.Visibility(EVisibility::Collapsed)
+		//		+ SHeaderRow::Column(TEXT("Socket"))
+		//	);
+		;
+
+	return BuildDesignTimeWidget(MyTreeView.ToSharedRef());
+}
+
+void UJavascriptTreeView::ProcessEvent(UFunction* Function, void* Parms)
+{
+	if (JavascriptContext && JavascriptContext->CallProxyFunction(this, this, Function, Parms))
+	{
+		return;
+	}
+
+	Super::ProcessEvent(Function, Parms);
+}
+
+void UJavascriptTreeView::RequestTreeRefresh()
+{
+	if (MyTreeView.IsValid())
+	{
+		MyTreeView->RequestTreeRefresh();
+	}	
+}
+
+/**
+* Implements a row widget for the session console log.
+*/
+class SJavascriptTableRow
+	: public SMultiColumnTableRow<UObject*>
+{
+public:
+	SLATE_BEGIN_ARGS(SJavascriptTableRow) { }
+		SLATE_ARGUMENT(UObject*, Object)
+		SLATE_ARGUMENT(UJavascriptTreeView*, TreeView)
+	SLATE_END_ARGS()
+
+public:
+
+	/**
+	* Constructs the widget.
+	*
+	* @param InArgs The construction arguments.
+	* @param InOwnerTableView The table view that owns this row.
+	*/
+	void Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView)
+	{
+		Object = InArgs._Object;
+		TreeView = InArgs._TreeView;
+
+		SMultiColumnTableRow<UObject*>::Construct(FSuperRowType::FArguments(), InOwnerTableView);
+	}
+
+public:
+
+	// SMultiColumnTableRow interface
+
+	BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+	virtual TSharedRef<SWidget> GenerateWidgetForColumn(const FName& ColumnName) override
+	{
+		auto ColumnWidget = SNullWidget::NullWidget;
+
+		if (TreeView->OnGenerateRowEvent.IsBound())
+		{
+			UWidget* Widget = TreeView->OnGenerateRowEvent.Execute(Object, ColumnName, TreeView);
+
+			if (Widget)
+			{
+				ColumnWidget = Widget->TakeWidget();
+			}
+		}
+
+		if (ColumnName == TreeView->Columns[0].Id)
+		{
+			// The first column gets the tree expansion arrow for this row
+			return
+				SNew(SHorizontalBox)
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				[
+					SNew(SExpanderArrow, SharedThis(this))
+				]
+
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				[
+					ColumnWidget
+				];
+		}
+		else
+		{
+			return ColumnWidget;
+		}
+	}
+	END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+private:
+	UObject* Object;
+	UJavascriptTreeView* TreeView;
+};
+
+TSharedRef<ITableRow> UJavascriptTreeView::HandleOnGenerateRow(UObject* Item, const TSharedRef< STableViewBase >& OwnerTable)
+{
+	// Call the user's delegate to see if they want to generate a custom widget bound to the data source.
+	if (OnGenerateRowEvent.IsBound())
+	{
+		if (Columns.Num())
+		{
+			return SNew(SJavascriptTableRow, OwnerTable).Object(Item).TreeView(this);
+		}
+		else
+		{
+			UWidget* Widget = OnGenerateRowEvent.Execute(Item, FName(), this);
+			if (Widget != NULL)
+			{
+				return SNew(STableRow< UObject* >, OwnerTable)
+					[
+						Widget->TakeWidget()
+					];
+			}
+		}		
+	}
+
+	// If a row wasn't generated just create the default one, a simple text block of the item's name.
+	return SNew(STableRow< UObject* >, OwnerTable)
+		[
+			SNew(STextBlock).Text(Item ? FText::FromString(Item->GetName()) : FText::FromName(FName()))
+		];
+}
+
+
+void UJavascriptTreeView::HandleOnGetChildren(UObject* Item, TArray<UObject*>& OutChildItems)
+{
+	if (OnGetChildren.IsBound())
+	{
+		Children.Empty();
+
+		OnGetChildren.Execute(Item,this);
+		
+		OutChildItems.Append(Children);
+
+		Children.Empty();
+	}
+}
+
+
+void UJavascriptTreeView::GetSelectedItems(TArray<UObject*>& OutItems)
+{
+	if (MyTreeView.IsValid())
+	{
+		OutItems = MyTreeView->GetSelectedItems();
+	}
+}
---
+++ Source/JavascriptUMG/JavascriptTreeView.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "JavascriptTreeView.generated.h"
+
+class UJavascriptContext;
+
+USTRUCT()
+struct FJavascriptColumn
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FName Id;
+
+	UPROPERTY()
+	float Width;
+
+	UPROPERTY(Transient)
+	UWidget* Widget;
+};
+
+/**
+* Allows thousands of items to be displayed in a list.  Generates widgets dynamically for each item.
+*/
+UCLASS(Experimental)
+class JAVASCRIPTUMG_API UJavascriptTreeView : public UTableViewBase
+{
+	GENERATED_UCLASS_BODY()
+
+public:	
+	/** Delegate for constructing a UWidget based on a UObject */
+	DECLARE_DYNAMIC_DELEGATE_TwoParams(FOnGetChildrenUObject, UObject*, Item, UJavascriptTreeView*, Instance);
+
+	/** Delegate for constructing a UWidget based on a UObject */
+	DECLARE_DYNAMIC_DELEGATE_RetVal_TwoParams(UWidget*, FOnGetColumn, FName, Id, UJavascriptTreeView*, Instance);
+
+	/** Delegate for constructing a UWidget based on a UObject */
+	DECLARE_DYNAMIC_DELEGATE_RetVal_ThreeParams(UWidget*, FOnGenerateRow, UObject*, Object, FName, Id, UJavascriptTreeView*, Instance);
+
+	/** Called when a widget needs to be generated */
+	UPROPERTY(EditAnywhere, Category = Events, meta = (IsBindableEvent = "True"))
+	FOnGenerateRow OnGenerateRowEvent;
+
+	/** Called when a widget needs to be generated */
+	UPROPERTY(EditAnywhere, Category = Events, meta = (IsBindableEvent = "True"))
+	FOnGetChildrenUObject OnGetChildren;	
+
+	/** Called when a widget needs to be generated */
+	UPROPERTY(EditAnywhere, Category = Events, meta = (IsBindableEvent = "True"))
+	FOnGetColumn OnGetColumn;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Javascript")
+	UJavascriptContext* JavascriptContext;
+
+	/** The list of items to generate widgets for */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Content)
+	TArray<UObject*> Items;
+
+	/** The selection method for the list */
+	UPROPERTY(EditAnywhere, Category = Content)
+	TEnumAsByte<ESelectionMode::Type> SelectionMode;
+
+	UPROPERTY(BlueprintReadWrite, Transient, Category = "Javascript")
+	mutable TArray<UObject*> Children;
+
+	UPROPERTY(BlueprintReadWrite, Transient, Category = "Javascript")
+	TArray<FJavascriptColumn> Columns;
+
+	/** Refreshes the list */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	void RequestTreeRefresh();
+
+	/** Event fired when a tutorial stage ends */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Javascript")
+	void OnDoubleClick(UObject* Object);
+
+	/** Event fired when a tutorial stage ends */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Javascript")
+	void OnSelectionChanged(UObject* Object, ESelectInfo::Type Type);
+
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	void GetSelectedItems(TArray<UObject*>& OutItems);
+
+	TSharedRef<ITableRow> HandleOnGenerateRow(UObject* Item, const TSharedRef< STableViewBase >& OwnerTable);
+
+	void HandleOnGetChildren(UObject* Item, TArray<UObject*>& OutChildItems);
+
+	// UWidget interface
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	// End of UWidget interface
+
+	// UObject interface
+	virtual void ProcessEvent(UFunction* Function, void* Parms) override;
+	// End of UObject interface
+
+	TSharedPtr< STreeView<UObject*> > MyTreeView;
+};
