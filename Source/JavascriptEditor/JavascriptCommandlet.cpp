7c7720d67f38fb23dc24af6c8026664df09873e7
Nako Sung <deif79@gmail.com>
2016-07-28 06:46:40 +0900

    Add javascript commandlet

---
+++ Source/JavascriptEditor/JavascriptCommandlet.cpp
@@ -0,0 +1,44 @@
+#include "JavascriptEditor.h"
+#include "JavascriptCommandlet.h"
+#include "JavascriptIsolate.h"
+#include "JavascriptContext.h"
+
+UJavascriptCommandlet::UJavascriptCommandlet(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{}
+
+int32 UJavascriptCommandlet::Main(const FString& Params)
+{
+	bool bSuccess = false;
+
+#if !UE_BUILD_SHIPPING
+	const TCHAR* ParamStr = *Params;
+	ParseCommandLine(ParamStr, CmdLineTokens, CmdLineSwitches);	
+
+	{
+		auto JavascriptIsolate = NewObject<UJavascriptIsolate>();
+		auto JavascriptContext = JavascriptIsolate->CreateContext();
+				
+		JavascriptContext->AddToRoot();
+
+		JavascriptContext->SetContextId(TEXT("Commandlet"));
+		
+		{
+			FEditorScriptExecutionGuard ScriptGuard;
+
+			if (CmdLineTokens.Num())
+			{
+				JavascriptContext->RunFile(CmdLineTokens[0]);
+			}
+		}
+
+		JavascriptContext->JavascriptContext.Reset();
+
+		JavascriptContext->RemoveFromRoot();
+	}
+#else	
+	bSuccess = false;
+#endif
+
+	return bSuccess ? 0 : 1;
+}
