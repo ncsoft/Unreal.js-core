5f7e57a0ffa5c9779978ac385b13c881ca67d80b
Nako Sung <deif79@gmail.com>
2016-07-30 14:01:14 +0900

    Fixes to execute only valid editor extension cleanup function

--- Content/Scripts/editor.js
+++ Content/Scripts/editor.js
@@ -34,7 +34,8 @@
 		}
 		
 		function main() {
-			let byes = extensions.map((what) => spawn(what))		
+		    let byes = _.filter(extensions.map((what) => spawn(what)),x => _.isFunction(x))
+
 			return function () {
 				byes.forEach((bye)=>bye())
 			}
