430a4b23d2c4c6c24a59bbfbdc5a76973443fd7f
Nako Sung <deif79@gmail.com>
2017-02-01 07:44:17 +0900

    Add public dependency: libWebSockets

--- Source/JavascriptWebSocket/JavascriptWebSocket.Build.cs
+++ Source/JavascriptWebSocket/JavascriptWebSocket.Build.cs
@@ -41,6 +41,7 @@ public class JavascriptWebSocket : ModuleRules
         if (IsUpdated() && (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32 || Target.Platform == UnrealTargetPlatform.Mac))
         {
             Definitions.Add(string.Format("WITH_JSWEBSOCKET=1"));
+            PublicDependencyModuleNames.Add("libWebSockets");
             AddEngineThirdPartyPrivateStaticDependencies(Target,"libWebSockets");
         }
         else
