4c8602d38003a1ea7914f9e50b12d28f6303d200
Crimson <443971798@qq.com>
2022-04-23 02:16:46 +0800

    重构UJavascriptListView
    1. 修改继承关系，使其直接从UListViewBase派生
    2. 清除RowItem的默认Style，因为默认Style会带有背景色

--- Source/JavascriptUMG/JavascriptListView.cpp
+++ Source/JavascriptUMG/JavascriptListView.cpp
@@ -1,36 +1,107 @@
 ﻿#include "JavascriptListView.h"
 #include "JavascriptContext.h"
 
+FTableRowStyle _TableRowStyle;
+
 UJavascriptListView::UJavascriptListView(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
-{	
+	: Super(ObjectInitializer)
+{
+	WidgetStyle = FUMGCoreStyle::Get().GetWidgetStyle<FTableViewStyle>("ListView");
+	ScrollBarStyle = FUMGCoreStyle::Get().GetWidgetStyle<FScrollBarStyle>("Scrollbar");
+
+	// 清除RowItem的默认Style
+	FSlateBrush Brush;
+	Brush.DrawAs = ESlateBrushDrawType::NoDrawType;
+	_TableRowStyle.ActiveBrush = Brush;
+	_TableRowStyle.ActiveHighlightedBrush = Brush;
+	_TableRowStyle.ActiveHoveredBrush = Brush;
+	_TableRowStyle.DropIndicator_Above = Brush;
+	_TableRowStyle.DropIndicator_Below = Brush;
+	_TableRowStyle.DropIndicator_Onto = Brush;
+	_TableRowStyle.EvenRowBackgroundBrush = Brush;
+	_TableRowStyle.EvenRowBackgroundHoveredBrush = Brush;
+	_TableRowStyle.InactiveBrush = Brush;
+	_TableRowStyle.InactiveHighlightedBrush = Brush;
+	_TableRowStyle.InactiveHoveredBrush = Brush;
+	_TableRowStyle.OddRowBackgroundBrush = Brush;
+	_TableRowStyle.OddRowBackgroundHoveredBrush = Brush;
+	_TableRowStyle.ParentRowBackgroundBrush = Brush;
+	_TableRowStyle.ParentRowBackgroundHoveredBrush = Brush;
+	_TableRowStyle.SelectorFocusedBrush = Brush;
+}
+
+void UJavascriptListView::AddReferencedObjects(UObject* InThis, FReferenceCollector& Collector)
+{
+	auto This = static_cast<UJavascriptListView*>(InThis);
+
+	if (This->MyListView.IsValid())
+	{
+		for (auto It = This->CachedRows.CreateIterator(); It; ++It)
+		{
+			auto Key = It->Key;
+			auto Value = It->Value;
+
+			if (Value.IsValid())
+			{
+				Collector.AddReferencedObject(Key, This);
+			}
+			else
+			{
+				It.RemoveCurrent();
+			}
+		}
+	}
+	else
+	{
+		This->CachedRows.Empty();
+	}
+
+	Super::AddReferencedObjects(This, Collector);
+}
+
+void UJavascriptListView::ProcessEvent(UFunction* Function, void* Parms)
+{
+	if (JavascriptContext && JavascriptContext->CallProxyFunction(this, this, Function, Parms))
+	{
+		return;
+	}
+
+	Super::ProcessEvent(Function, Parms);
+}
+
+void UJavascriptListView::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+
+	MyListView.Reset();
 }
 
 TSharedRef<STableViewBase> UJavascriptListView::RebuildListWidget()
 {
 	SAssignNew(MyListView, SJavascriptListView)
+		.ListViewStyle(&WidgetStyle)
+		.ScrollBarStyle(&ScrollBarStyle)
 		.SelectionMode(SelectionMode)
 		.ListItemsSource(&Items)
 		.ItemHeight(ItemHeight)
-		.OnGenerateRow(BIND_UOBJECT_DELEGATE(SListView< UObject* >::FOnGenerateRow, HandleOnGenerateRow))
+		.OnGenerateRow(BIND_UOBJECT_DELEGATE(SListView<UObject*>::FOnGenerateRow, HandleOnGenerateRow))
 		.OnSelectionChanged_Lambda([this](UObject* Object, ESelectInfo::Type SelectInfo)
-		{
-			OnSelectionChanged(Object, SelectInfo);
-		})
+			{
+				OnSelectionChanged(Object, SelectInfo);
+			})
 		.OnMouseButtonClick_Lambda([this](UObject* Object)
-		{
-			OnClick(Object);
-		})
+			{
+				OnClick(Object);
+			})
 		.OnMouseButtonDoubleClick_Lambda([this](UObject* Object)
-		{
-			OnDoubleClick(Object);
-		})
-		.HeaderRow(GetHeaderRowWidget())
-		.ExternalScrollbar(SNew(SScrollBar).Style(&ScrollBarStyle));
+			{
+				OnDoubleClick(Object);
+			});
 
+	// 支持右键打开菜单，但目前右键菜单会默认带有一个背景层
 	if (OnContextMenuOpening.IsBound())
 	{
-		MyListView->SetOnContextMenuOpening(::FOnContextMenuOpening::CreateLambda([this]()
+		MyListView->SetOnContextMenuOpening(FOnContextMenuOpening::CreateLambda([this]()
 		{
 			if (OnContextMenuOpening.IsBound())
 			{
@@ -45,6 +116,39 @@ TSharedRef<STableViewBase> UJavascriptListView::RebuildListWidget()
 	}
 
 	return MyListView.ToSharedRef();
+}
+
+TSharedRef<ITableRow> UJavascriptListView::HandleOnGenerateRow(UObject* Item, const TSharedRef<STableViewBase>& OwnerTable)
+{
+	// Call the user's delegate to see if they want to generate a custom widget bound to the data source.
+	if (OnGenerateRow.IsBound())
+	{
+		UWidget* Widget = OnGenerateRow.Execute(Item, this);
+		if (Widget != NULL)
+		{
+			return CreateItemRow(Widget, OwnerTable);
+		}
+	}
+
+	// If a row wasn't generated just create the default one, a simple text block of the item's name.
+	return CreateDefaultRow(Item, OwnerTable);
+}
+
+TSharedRef<ITableRow> UJavascriptListView::CreateDefaultRow(UObject* Item, const TSharedRef<STableViewBase>& OwnerTable)
+{
+	return SNew(STableRow<UObject*>, OwnerTable)
+		[
+			SNew(STextBlock).Text(Item ? FText::FromString(Item->GetName()) : FText::FromName(FName()))
+		]
+	.Style(&_TableRowStyle);
+}
+
+TSharedRef<ITableRow> UJavascriptListView::CreateItemRow(UWidget* Widget, const TSharedRef<STableViewBase>& OwnerTable)
+{
+	auto GeneratedWidget = Widget->TakeWidget();
+	CachedRows.Add(Widget, GeneratedWidget);
+	return SNew(STableRow<UObject*>, OwnerTable)[GeneratedWidget]
+		.Style(&_TableRowStyle);
 }
 
 void UJavascriptListView::RequestListRefresh()
@@ -72,34 +176,26 @@ void UJavascriptListView::SetSelection_Implementation(UObject* SoleSelectedItem)
 	}
 }
 
-void UJavascriptListView::ProcessEvent(UFunction* Function, void* Parms)
+void UJavascriptListView::SetItemSelection(TArray<UObject*> MultiSelectedItems, bool bIsSelected)
 {
-	if (JavascriptContext && JavascriptContext->CallProxyFunction(this, this, Function, Parms))
+	if (MyListView.IsValid())
 	{
-		return;
+		MyListView->SetItemSelection(MultiSelectedItems, bIsSelected);
 	}
-
-	Super::ProcessEvent(Function, Parms);
 }
 
-void UJavascriptListView::ReleaseSlateResources(bool bReleaseChildren)
+void UJavascriptListView::ClearSelection()
 {
-	Super::ReleaseSlateResources(bReleaseChildren);
-
-	MyListView.Reset();
+	if (MyListView.IsValid())
+	{
+		MyListView->ClearSelection();
+	}
 }
 
-TSharedRef<ITableRow> UJavascriptListView::CreateItemRow(UWidget* Widget, const TSharedRef<STableViewBase>& OwnerTable)
+void UJavascriptListView::RequestNavigateToItem(UObject* Item)
 {
-	auto GeneratedWidget = Widget->TakeWidget();
-	CachedRows.Add(Widget, GeneratedWidget);
-	return SNew(STableRow<UObject*>, OwnerTable)[GeneratedWidget];
-}
-
-TSharedRef<ITableRow> UJavascriptListView::CreateDefaultRow(UObject* Item, const TSharedRef<STableViewBase>& OwnerTable)
-{
-	return SNew(STableRow<UObject*>, OwnerTable)
-		[
-			SNew(STextBlock).Text(Item ? FText::FromString(Item->GetName()) : FText::FromName(FName()))
-		];
+	if (MyListView.IsValid())
+	{
+		MyListView->RequestNavigateToItem(Item);
+	}
 }
--- Source/JavascriptUMG/JavascriptListView.h
+++ Source/JavascriptUMG/JavascriptListView.h
@@ -1,13 +1,14 @@
 ﻿#pragma once
 
-#include "JavascriptTreeView.h"
+#include "Widgets/Views/SListView.h"
+#include "Components/ListViewBase.h"
 #include "JavascriptListView.generated.h"
 
 class SJavascriptListView : public SListView<UObject*>
 {
 public:
 	SJavascriptListView()
-		: SListView<UObject*>()
+	: SListView<UObject*>()
 	{
 	}
 
@@ -20,33 +21,23 @@ public:
 
 class UJavascriptContext;
 
+/** Delegate for constructing a UWidget based on a UObject */
+DECLARE_DYNAMIC_DELEGATE_RetVal_TwoParams(UWidget*, FOnGenerateRowSignature, UObject*, Object, UJavascriptListView*, Instance);
+
+/** Delegate for constructing a UWidget based on a UObject */
+DECLARE_DYNAMIC_DELEGATE_RetVal_OneParam(UWidget*, FOnContextMenuOpeningSignature, UJavascriptListView*, Instance);
+
 /**
 * Allows thousands of items to be displayed in a list.  Generates widgets dynamically for each item.
 */
 UCLASS(Experimental)
-class JAVASCRIPTUMG_API UJavascriptListView : public UJavascriptTreeView
+class JAVASCRIPTUMG_API UJavascriptListView : public UListViewBase
 {
 	GENERATED_UCLASS_BODY()
 
-public:	
-	/** The height of each widget */
-	UPROPERTY(EditAnywhere, Category = Content)
-	float ItemHeight;
-
-	/** Event fired when a tutorial stage ends */
-	UFUNCTION(BlueprintImplementableEvent, Category = "Javascript")
-	void OnClick(UObject* Object);
-
-	/** Refreshes the list */
-	UFUNCTION(BlueprintCallable, Category = "Behavior")
-	void RequestListRefresh();
-
-	virtual void SetSelection_Implementation(UObject* SoleSelectedItem) override;
-	virtual bool GetSelectedItems_Implementation(TArray<UObject*>& OutItems) override;
-
-	// UWidget interface
-	virtual TSharedRef<STableViewBase> RebuildListWidget() override;
-	// End of UWidget interface
+public:
+	// UObject interface.
+	static void AddReferencedObjects(UObject* InThis, FReferenceCollector& Collector);
 
 	// UObject interface
 	virtual void ProcessEvent(UFunction* Function, void* Parms) override;
@@ -56,9 +47,76 @@ public:
 	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
 	//~ End UVisual Interface
 
+	// UWidget interface
+	virtual TSharedRef<STableViewBase> RebuildListWidget() override;
+	// End of UWidget interface
+
+	TSharedRef<ITableRow> HandleOnGenerateRow(UObject* Item, const TSharedRef< STableViewBase >& OwnerTable);
+
+	virtual TSharedRef<ITableRow> CreateDefaultRow(UObject* Item, const TSharedRef<STableViewBase>& OwnerTable);
+	virtual TSharedRef<ITableRow> CreateItemRow(UWidget* Widget, const TSharedRef<STableViewBase>& OwnerTable);
+
+	/** Refreshes the list */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	void RequestListRefresh();
+
+	/** Event fired when a tutorial stage ends */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Javascript")
+	void OnSelectionChanged(UObject* Object, ESelectInfo::Type Type);
+
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	void SetItemSelection(TArray<UObject*> MultiSelectedItems, bool bIsSelected);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Behavior")
+	void SetSelection(UObject* SoleSelectedItem);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Behavior")
+	bool GetSelectedItems(TArray<UObject*>& OutItems);
+
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	void ClearSelection();
+
+	UFUNCTION(BlueprintCallable, Category = "Javascript")
+	void RequestNavigateToItem(UObject* Item);
+
+	/** Event fired when a tutorial stage ends */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Javascript")
+	void OnClick(UObject* Object);
+
+	/** Event fired when a tutorial stage ends */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Javascript")
+	void OnDoubleClick(UObject* Object);
+
+protected:
 	TSharedPtr<SJavascriptListView> MyListView;
 
-	// Overriden functions from UJavascriptTreeView
-	virtual TSharedRef<ITableRow> CreateItemRow(UWidget* Widget, const TSharedRef<STableViewBase>& OwnerTable) override;
-	virtual TSharedRef<ITableRow> CreateDefaultRow(UObject* Item, const TSharedRef<STableViewBase>& OwnerTable) override;
+	TMultiMap<UObject*, TWeakPtr<SWidget>> CachedRows;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Javascript")
+	UJavascriptContext* JavascriptContext;
+
+	/** The list of items to generate widgets for */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Content)
+	TArray<UObject*> Items;
+
+	/** The selection method for the list */
+	UPROPERTY(EditAnywhere, Category = ListView)
+	TEnumAsByte<ESelectionMode::Type> SelectionMode = ESelectionMode::Single;;
+
+	/** The height of each widget */
+	UPROPERTY(EditAnywhere, Category = ListView)
+	float ItemHeight;
+
+	/** Called when a widget needs to be generated */
+	UPROPERTY(EditAnywhere, Category = Events, meta = (IsBindableEvent = "True"))
+	FOnGenerateRowSignature OnGenerateRow;
+
+	UPROPERTY(EditAnywhere, Category = Events, meta = (IsBindableEvent = "True"))
+	FOnContextMenuOpeningSignature OnContextMenuOpening;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = ListView)
+	FTableViewStyle WidgetStyle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = ListView)
+	FScrollBarStyle ScrollBarStyle;
 };
